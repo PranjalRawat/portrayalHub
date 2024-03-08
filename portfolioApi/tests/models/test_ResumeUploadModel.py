467503064e398c22553ba27207cfc27ac8c53817
pranjal rawat <rawatpranjal111@gmail.com>
2024-03-08 13:59:24 +0530

    Added Testcases for Resume Upload Model

---
+++ portfolioApi/tests/models/test_ResumeUploadModel.py
@@ -0,0 +1,76 @@
+from django.test import TestCase
+from django.contrib.auth.models import User
+from portfolioApi.models import UserProfileModel, ResumeUploadModel
+from faker import Faker
+
+fake = Faker()
+
+# Test Execution Command :::>>>  python3 manage.py test portfolioApi.tests.models.test_ResumeUploadModel
+class ResumeUploadModelTest(TestCase):
+
+    @classmethod
+    def setUpClass(self):
+        super().setUpClass()
+
+        self.user_data = {
+            'username': fake.user_name(),
+            'email': fake.email(),
+            'password': fake.password(),
+        }
+        self.userInstance = User.objects.create(**self.user_data)
+
+        self.user_profile = {
+            'user' : self.userInstance,
+            'first_name' : fake.first_name(),
+            'last_name': fake.last_name(),
+            'date_of_birth': fake.date_of_birth(),
+            'gender': fake.random_element(elements = ('M', 'F', 'O')),
+            'address': fake.address(),
+            'email': fake.email(),
+            'phone_number': fake.phone_number(),
+        }
+        self.userProfileInstance = UserProfileModel.objects.create(**self.user_profile)
+
+        self.user_resume_data = {
+            'user_profile': self.userProfileInstance,
+            'resume': fake.file_path(extension='pdf', category='document', depth=1),
+            'video_resume': fake.file_path(extension='mp4', category='video', depth=1),
+        }
+
+        self.userResumeInstance = ResumeUploadModel.objects.create(**self.user_resume_data)
+
+    def test_user_resume_str_method(self):
+        user_profile_resume = ResumeUploadModel.objects.get(id=1)
+        self.assertEqual(str(user_profile_resume), f"{user_profile_resume.user_profile.user.username}'s Profile Resume")
+
+    def test_user_single_profile_resume_instance(self):
+        with self.assertRaises(Exception) as context:
+            # Attempt to create another user resume instance for the same user profile
+            ResumeUploadModel.objects.create(
+                user_profile = self.userProfileInstance,
+                resume = fake.file_path(extension='pdf', category='document', depth=1),
+                video_resume = fake.file_path(extension='mp4', category='video', depth=1),
+            )
+            self.assertTrue('unique constraint' in str(context.exception))
+
+    def test_user_profile_resume_deletion_with_user(self):
+        user_profile_resume = ResumeUploadModel.objects.get(id=1)
+        self.assertTrue(ResumeUploadModel.objects.filter(user_profile=user_profile_resume.user_profile).exists())
+        user_profile_resume.user_profile.user.delete()
+        self.assertFalse(ResumeUploadModel.objects.filter(user_profile=user_profile_resume.user_profile).exists())
+
+    def test_user_profile_resume_deletion_with_direct_delete(self):
+        user_profile_resume = ResumeUploadModel.objects.get(id=1)
+        self.assertTrue(ResumeUploadModel.objects.filter(user_profile=user_profile_resume.user_profile).exists())
+        user_profile_resume.delete()
+        self.assertFalse(ResumeUploadModel.objects.filter(user_profile=user_profile_resume.user_profile).exists())
+
+    def test_user_profile_resume_document_resume_validity(self):
+        user_profile_resume = ResumeUploadModel.objects.get(id=1)
+        self.assertTrue(user_profile_resume.resume == self.user_resume_data['resume'])
+        self.assertTrue(user_profile_resume.resume.url.endswith('.pdf'))
+
+    def test_user_profile_resume_video_resume_validity(self):
+        user_profile_resume = ResumeUploadModel.objects.get(id=1)
+        self.assertTrue(user_profile_resume.video_resume == self.user_resume_data['video_resume'])
+        self.assertTrue(user_profile_resume.video_resume.url.endswith('.mp4'))
