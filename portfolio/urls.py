73364e70457a585676a1c227b5be10e052a65e8a
pranjal rawat <rawatpranjal111@gmail.com>
2024-04-03 15:19:06 +0530

    update dashboard Url

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.DashboardView.as_view(), name='home'),
+    path('<str:username>/', views.DashboardView.as_view(), name='home'),
     # Define more web app routes here as needed
 ]
--- portfolio/views.py
+++ portfolio/views.py
@@ -1,16 +1,20 @@
-from django.views.generic import ListView
+from django.views.generic import DetailView
 from portfolio.forms import ContactForm
 from portfolioApi.models import CertificateInfoModel, EducationInfoModel, ExperienceInfoModel, MajorProjectsInfoModel, ProfileImageModel, ResumeUploadModel, SkillsInfoModel, SocialPlatformsModel, UserProfileModel
 from django.shortcuts import redirect
 from django.core.mail import send_mail
 from portrayalHub.settings import Email_from, Email_to
+from django.contrib.auth.models import User
+from django.http import Http404
 
 # Create your views here.
-class DashboardView(ListView):
+class DashboardView(DetailView):
     template_name = 'dashboard.html'
     context_object_name = 'data'
     queryset = UserProfileModel.objects.all()
     form_class = ContactForm
+    slug_field = 'user'
+    slug_url_kwarg = 'username'
 
     def post(self, request):
         form = ContactForm(request.POST)
@@ -29,8 +33,17 @@ class DashboardView(ListView):
             )
         return redirect('/')
 
+    def get_object(self, queryset=None):
+        username = self.kwargs.get('username', None)
+        try:
+            user = User.objects.get(username=username)
+        except User.DoesNotExist:
+            raise Http404("User does not exist")
+        return user
+
     def get_context_data(self, **kwargs):
         context = super(DashboardView, self).get_context_data(**kwargs)
+        context['UserProfile'] = UserProfileModel.objects.all()
         context['ProfileImage'] = ProfileImageModel.objects.all()
         context['ResumeUpload'] = ResumeUploadModel.objects.all()
         context['SocialPlatforms'] = SocialPlatformsModel.objects.filter(featured = True)
