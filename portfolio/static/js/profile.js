9750719640f7398e99efc76080e75016cf73cef6
pranjal rawat <rawatpranjal111@gmail.com>
2024-04-17 11:09:49 +0530

    Update Profile page navigation section

--- portfolio/static/js/profile.js
+++ portfolio/static/js/profile.js
@@ -1,10 +1,20 @@
 let sidebar = document.querySelector(".sidebar");
 let closeBtn = document.querySelector("#btn");
+let sidebarNavTitle = document.querySelectorAll(".sidebar .nav-list a");
 
 closeBtn.addEventListener("click", () => {
     sidebar.classList.toggle("open");
     menuBtnChange();
 });
+
+for (let [idx, _] of sidebarNavTitle.entries()) {
+    sidebarNavTitle[idx].addEventListener("click", () => {
+        if (sidebar.classList.contains("open")) {
+            sidebar.classList.toggle("open");
+            menuBtnChange();
+        }
+    });
+}
 
 function menuBtnChange() {
     if (sidebar.classList.contains("open")) {
