aac76ca2526547d82a49705e09aa1dbfd2a2fa5b
pranjal rawat <rawatpranjal111@gmail.com>
2024-04-18 11:56:19 +0530

    Update Profile view for different resolutions

--- portfolio/static/js/profile.js
+++ portfolio/static/js/profile.js
@@ -1,17 +1,17 @@
 let sidebar = document.querySelector(".sidebar");
+let homeSection = document.querySelector(".home-section")
 let closeBtn = document.querySelector("#btn");
 let sidebarNavTitle = document.querySelectorAll(".sidebar .nav-list a");
 
 closeBtn.addEventListener("click", () => {
-    sidebar.classList.toggle("open");
-    menuBtnChange();
+    toggleSidebarAndHomeSection();
 });
 
 for (let [idx, _] of sidebarNavTitle.entries()) {
+    const width = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
     sidebarNavTitle[idx].addEventListener("click", () => {
-        if (sidebar.classList.contains("open")) {
-            sidebar.classList.toggle("open");
-            menuBtnChange();
+        if (sidebar.classList.contains("open") && width < 986) {
+            toggleSidebarAndHomeSection();
         }
     });
 }
@@ -23,3 +23,19 @@ function menuBtnChange() {
         closeBtn.classList.replace("bx-menu-alt-right", "bx-menu");
     }
 }
+
+function toggleSidebarAndHomeSection() {
+    sidebar.classList.toggle("open");
+    homeSection.classList.toggle("sidebarOpen")
+    menuBtnChange();
+}
+
+function addClassIfWidthGreaterThanEqual986() {
+    const width = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+
+    if (width >= 986) {
+        toggleSidebarAndHomeSection();
+    }
+}
+
+addClassIfWidthGreaterThanEqual986();
